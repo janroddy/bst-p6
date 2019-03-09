dc677974a322b6f53d92e8f7cab94be1c79d1959
Levi Terry <ldterry14@gmail.com>
2019-03-09 14:16:12 -0800

    Made minor edits to test script

--- test_script.sh
+++ test_script.sh
@@ -2,12 +2,11 @@
 
 ./myprog.exe myfile1.txt > file1
 
-result="$(diff f1 file1)"
+result1=$(diff f1 file1)
 
 # repeat for myfile2.txt > file2
 # create f1, f2 etc with what the results should be
-if [$? -eq 0]
-then
+if [ -z "$result1" ]; then
 	echo "Test 1 success"
 else
 	echo "Test 1 failed"
@@ -16,10 +15,9 @@ fi
 
 ./myprog.exe myfile2.txt > file2
 
-result="$(diff f2 file2)"
+result2=$(diff f2 file2)
 
-if [$? -eq 0]
-then
+if [ -z "$result2" ]; then
 	echo "Test 2 success"
 else
 	echo "Test 2 failed"
@@ -28,10 +26,9 @@ fi
 
 ./myprog.exe myfile3.txt > file3
 
-result="$(diff f3 file3)"
+result3=$(diff f3 file3)
 
-if [$? -eq 0]
-then
+if [ -z "$result3" ]; then
 	echo "Test 3 success"
 else
 	echo "Test 3 failed"
