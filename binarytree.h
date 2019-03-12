a041e000726d627ec47ca0aa0de0e37ab64c8932
witsampson <witsampson@gmail.com>
2019-03-12 15:01:06 -0600

    tree height revision

--- binarytree.h
+++ binarytree.h
@@ -15,6 +15,6 @@ void printinorder(binary_tree* bt);
 void printpreorder(binary_tree* bt);
 void printpostorder(binary_tree* bt);
 int btsize(binary_tree* bt);
-int treeheight(binary_tree* bt); 
+int treeheight(node* root); 
 
 #endif 
